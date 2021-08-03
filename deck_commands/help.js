bba36836dd7bba9b97281b3096455cfe7f655935
Aaron Yu <aaronyu3@illinois.edu>
2021-08-03 01:09:25 -0700

    formatting help a bit more.

--- deck_commands/help.js
+++ deck_commands/help.js
@@ -19,7 +19,7 @@ module.exports = {
       const data = [];
 
 		if (!args.length) {
-      data.push('__**Here\'s a list of all my commands:**__\n`');
+      data.push('__**Here\'s a list of all my commands:**__');
       data.push(commands.map(command => command.name).join('`\n`'));
       data.push(`\`\nYou can send \`${prefix}help <command name>\` to get info on a specific command!`);
       return message.author.send(data, { split: true })
